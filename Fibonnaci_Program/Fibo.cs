4c285e6324d381100b61bc11aed4b1609dbc83be
Theju03 <Theju@0383$>
2020-12-02 23:44:45 +0000

    Initial check-in of module Fibonnaci_Program

---
+++ Fibonnaci_Program/Fibo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+  
+namespace Fibonacci_Program
+{
+    /// <summary>Class <c> MainClass </c> models for finding the nth Number of the Fibonacci Series plane.</summary>
+    class Fibo
+    {
+        /// <summary>This is the entry point of the Fibonacci class  program. </summary>
+        public static void Main(string[] args)
+        {
+            long value;
+            long inputnum;
+
+            Console.WriteLine("Enter the number: ");
+            inputnum = ValidInputCheck(Console.ReadLine());
+
+            value = Fibonacci(inputnum);
+            Console.WriteLine($"The {inputnum}th number in fibonacci series is {value}");
+        }
+
+
+        /// <summary>This is the method for validating the input </summary>
+        private static long ValidInputCheck(string val)
+        {
+            long inputValue;
+            bool success = long.TryParse(val, out inputValue);
+            if (success && Convert.ToInt64(val) > 0)
+            {
+                return inputValue;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        /// <summary>This is the method for calculating the nth Fibonacci number </summary>
+        private static long Fibonacci(long input)
+        {
+
+            if (input <= 1)
+            {
+                return input;
+            }
+            else
+            {
+                return (Fibonacci(input - 1) + Fibonacci(input - 2));
+            }
+
+        }
+    }
+}
---
+++ Finonacci_program.UnitTests/FibonacciTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Finonacci_program.UnitTests
+{
+    [TestClass]
+    public class FibonacciTests
+    {
+        [TestMethod]
+        public void ValidInputCheckTests_AlphabetInput_ReturnsInvalid()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
